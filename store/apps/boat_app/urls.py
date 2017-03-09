8728f7a4f768a854911c4bb48ac1b4befb1e5c2f
Alex Ambrose <ambrose@purdue.edu>
2017-03-09 16:32:38 -0600

    added content to sevices container

--- store/apps/boat_app/urls.py
+++ store/apps/boat_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index), #loads index page
     url(r'^contact/$', views.contact), #loads contact page
-    url(r'^contact/submit$', views.submit_contact_form) #runs submit_contact_form function in controller
+    url(r'^contact/submit$', views.submit_contact_form), #runs submit_contact_form function in controller
+    url(r'^test/$', views.test), #loads contact page
 ]
