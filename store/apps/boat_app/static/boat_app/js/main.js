18c9a4f7ec5e2c7c7ab7d4e2cbff10d9dc558e65
Your Name <you@example.com>
2017-04-05 15:14:40 -0500

    layout changes

--- store/apps/boat_app/static/boat_app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#validationform").submit(function(){
-        $("#formspree").submit();
-        console.log("form submitted");
-        return false;
-    });
-});
