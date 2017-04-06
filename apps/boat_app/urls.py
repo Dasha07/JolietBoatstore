30a4a90bbdeefbb2b75a6735e3b3961c771311c4
Your Name <you@example.com>
2017-04-05 19:11:06 -0500

    file changes

---
+++ apps/boat_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index), #loads index page
+    url(r'^contact/$', views.contact), #loads contact page
+    url(r'^contact/submit$', views.submit_contact_form), #runs submit_contact_form function in controller
+    url(r'^transport/$', views.transport), #loads transportation page
+    url(r'^shore/$', views.shore), #loads shore power page
+    url(r'^water_fuel/$', views.water_fuel), #loads water fuel page
+    url(r'^grocery_supply/$', views.groceries), #loads supplies and groceries page
+    url(r'^waste/$', views.waste) #loads waste page
+]
---
+++ apps/boat_app/views.py
@@ -0,0 +1,69 @@
+import re
+import smtplib
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import get_messages
+from django.core.exceptions import ValidationError
+
+from django.core.mail import send_mail
+from email.mime.text import MIMEText
+
+# Create your views here.
+def index(request):
+    return render(request, "boat_app/index.html")
+
+def contact(request):
+    if 'form' not in request.session:
+        request.session['form'] = {}
+
+    context = {
+        'form' : request.session['form']
+    }
+    return render(request, "boat_app/contact.html",context)
+
+def submit_contact_form(request):
+    form = {
+        'f_name': request.POST['f_name'],
+        'l_name': request.POST['l_name'],
+        'email': request.POST['email'],
+        'phone': request.POST['phone'],
+        'message': request.POST['message']
+    }
+    request.session['form'] = form
+    email_match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', form['email'])
+
+    phone_match = re.match('(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', form['phone'])
+
+    if len(form['f_name']) < 2 or len(form['l_name']) < 2:
+        messages.add_message(request, messages.INFO, 'First and Last Name must be at least 2 characters')
+
+    if not email_match:
+        messages.add_message(request, messages.INFO, 'Not a valid email address')
+
+    print "****** {} *******".format(form)
+    if form['phone'] and not phone_match:
+        messages.add_message(request, messages.INFO, 'Not a valid phone number - (e.g. XXX-XXX-XXXX)')
+
+    if not form['message']:
+        messages.add_message(request, messages.INFO, 'Please enter a message to be sent')
+
+    if get_messages(request):
+        return redirect('/#contact_us')
+
+    return redirect('/#contact_us')
+
+def transport(request):
+    return render(request, "boat_app/transport.html")
+
+def shore(request):
+    return render(request, "boat_app/shore_power.html")
+
+def water_fuel(request):
+    return render(request, "boat_app/water_fuel.html")
+
+def groceries(request):
+    return render(request, "boat_app/groceries.html")
+
+def waste(request):
+    return render(request, "boat_app/waste.html")
